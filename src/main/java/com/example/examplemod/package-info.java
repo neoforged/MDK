a37a2ac775ce979fa332ac085c5f39f03715445a
TeamDman <TeamDman9201@gmail.com>
2023-08-19 01:46:24 -0400

    Add package-info.java

---
+++ src/main/java/com/example/examplemod/package-info.java
@@ -0,0 +1,9 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package com.example.examplemod;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
